2aaab55b58797e42426ed83d5f162bd29a197655
SKPR <134925461+SKPR15298@users.noreply.github.com>
2026-07-21 09:47:12 +0700

    Update base path for production environment

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
       strict: true,
     }),
     paths: {
-      base: process.env.NODE_ENV === "production" ? "/ponpesirmus" : "",
+      base: process.env.NODE_ENV === "production" ? "/ponpesirsyam" : "",
     },
   },
 };
